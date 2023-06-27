faae81c3d95e9ab1361ffc2af82025fe4bb3968b
The Leo Developer <44649257+leodev12345@users.noreply.github.com>
2023-06-27 18:48:23 +0200

    fixed form bugs

--- app.py
+++ app.py
@@ -12,13 +12,17 @@ ilegal_charecters=[" ","#","<",">","$","+","%","!","`","&","*","|","{","}","[","
 #load json values into the local list every time the program starts
 f=open("database/data.json")
 data=json.load(f)
-config=open("database/config.json")
-config_data=json.load(config)
 for i in data:
     list.append(tuple((i[0],i[1],i[2],i[3])))
 
-for j in config_data:
-    config_list.append(j[0])
+with open("database/config.json", "r") as f:
+    config_data=json.load(f)
+
+config_path=config_data[1]
+config_ip=config_data[2]
+config_user=config_data[3]
+
+print(config_data)
 
 app = Flask(__name__)
 
@@ -38,28 +42,23 @@ def my_form_post():
     if text!="" and any(ele in text for ele in ilegal_charecters)==False:
         if desc=="":
             desc="No description"
-        copy_text="pi@192.168.1.116:/home/pi/git/"+text+".git"
+        copy_text=config_user+"@"+config_ip+":"+config_path+text+".git"
 
         #create the repo on the server
-        #os.system('cd /home/pi/git')
-        #os.system('mkdir '+text+'.git')
-        #os.system('cd '+text+'.git')
-        #os.system('git init --bare')
-        print("--------Commands simulation--------")
-        print('cd /home/pi/git')
-        print('mkdir '+text+'.git')
-        print('cd '+text+'.git')
-        print('git init --bare')
-        print("-----------------------------------")
-
+        #dir_name=text+".git"
+        #os.system("cd "+config_path)
+        #os.system("mkdir "+dir_name)
+        #os.system("cd "+dir_name)
+        #os.system("git init --bare")
+        
         #add info to list
-        list.insert(0,tuple((text,"Path: pi@192.168.1.116:/home/pi/git/"+text+".git",desc,copy_text)))
+        list.insert(0,tuple((text,"Path: "+copy_text,desc,copy_text)))
         write_json(list)
 
     #refresh
     return redirect(url_for('my_form_post'))
 
-#show index.html
+#show homepage
 @app.route("/")
 def template_test():
     if logged_in==True:
@@ -70,13 +69,17 @@ def template_test():
 #show more.html
 @app.route("/more")
 def more_render():
-    return render_template('more.html')
+    if logged_in==True:
+        return render_template('more.html')
+    elif logged_in==False:
+        return redirect(url_for('login_render'))
 
-#show login
+#show login.html
 @app.route("/login")
 def login_render():
     return render_template('login.html')
 
+#check if password is correct
 @app.route("/login", methods=['POST'])
 def login():
     password=request.form['password']
@@ -91,51 +94,61 @@ def login():
 
 @app.route("/more", methods=['POST'])
 def more():
-    old_name = request.form['old_name']
-    new_name = request.form['new_name']
-    delete_name = request.form['name_1']
-    delete_name_confirm = request.form['name_2']
-    repo_name = request.form['repo_name']
-    new_desc = request.form['new_desc']
 
     #renaming repos
-    if old_name!=new_name and new_name!="" and old_name!="" and " " not in old_name and " " not in new_name and any(ele in new_name for ele in ilegal_charecters)==False:
-        print("--------Commands simulation--------")
-        print('cd /home/pi/git')
-        print('mv '+old_name+'.git'+' '+new_name+'.git')
-        print("-----------------------------------")
-        result = [tup[0] for tup in list].index(old_name) #index of the tuple containing the old name
-        #change values
-        desc = list[result][2]
-        path="Path: pi@192.168.1.116:/home/pi/git/"+new_name+".git"
-        copy="pi@192.168.1.116:/home/pi/git/"+new_name+".git"
-        #write new values
-        list[result]=tuple((new_name,path,desc,copy))
-        write_json(list)
+    if 'rename' in request.form:
+        old_name = request.form['old_name']
+        new_name = request.form['new_name']
+        if old_name!=new_name and new_name!="" and old_name!="" and " " not in old_name and " " not in new_name and any(ele in new_name for ele in ilegal_charecters)==False:
+            #rename repo on server
+            #os.system("cd "+config_path)
+            #os.system("mv "+old_name+".git"+" "+new_name+".git")
+        
+            result = [tup[0] for tup in list].index(old_name) #index of the tuple containing the old name
+            #change values
+            desc = list[result][2]
+            path="Path: "+config_user+"@"+config_ip+":"+config_path+new_name+".git"
+            copy=config_user+"@"+config_ip+":"+config_path+new_name+".git"
+            #write new values
+            list[result]=tuple((new_name,path,desc,copy))
+            write_json(list)
 
     #deleting repos
-    if delete_name==delete_name_confirm and delete_name!="":
-        result = [tup[0] for tup in list].index(delete_name) #get index of the tuple that needs to be deleted
-        del list[result] #delete from list
-        write_json(list)
+    elif 'remove' in request.form:
+        delete_name = request.form['name_1']
+        delete_name_confirm = request.form['name_2']
+        if delete_name==delete_name_confirm and delete_name!="":
+            result = [tup[0] for tup in list].index(delete_name) #get index of the tuple that needs to be deleted
+            del list[result] #delete from list
+            #delete on device
+            #os.system("cd "+config_path)
+            #os.rmdir(delete_name+".git")
+            write_json(list)
 
     #changing repo description
-    if repo_name!="" and new_desc!="":
-        result = [tup[0] for tup in list].index(repo_name) #get the index of the tuple that contains the repo name
-        #get values that wont change
-        name=list[result][0]
-        path=list[result][1]
-        copy=list[result][3]
-        #write new values
-        list[result] = tuple((name,path,new_desc,copy))
-        write_json(list)
-    if request.form['log_out'] == "Log out":
+    elif 'confirm' in request.form:
+        repo_name = request.form['repo_name']
+        new_desc = request.form['new_desc']
+        if 'confirm' in request.form and repo_name!="" and new_desc!="":
+            result = [tup[0] for tup in list].index(repo_name) #get the index of the tuple that contains the repo name
+            #get values that wont change
+            name=list[result][0]
+            path=list[result][1]
+            copy=list[result][3]
+            #write new values
+            list[result] = tuple((name,path,new_desc,copy))
+            write_json(list)
+    
+    #logout
+    elif 'logout' in request.form:    
         global logged_in
         logged_in=False
-        return redirect(url_for('template_test'))
+        return redirect(url_for('login_render'))
+    
+    #to refresh website
     return render_template('more.html')
 
 
 #run
 if __name__ == '__main__':
-    app.run(debug=False, port=80, host='0.0.0.0')
+    app.run(debug=True, port=80, host='0.0.0.0')
--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import json
 import bcrypt
+import os
+import socket
 
 f=open("database/config.json")
 data=json.load(f)
@@ -14,12 +16,22 @@ def write_json(arg):
 
 #first time setup
 if len(list)==0:
-    print("Create password:")
-    password=input()
+    password=input("Create password: ")
     bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(bytes, salt)
     hash_json = hash.decode('utf8').replace("'", '"')
-    list.append(hash_json)
+    list.append(hash_json) 
+    print("Password set to "+password)
+    path=input("Set storage location for your repositories: ")
+    list.append(path)
+    print("Storage path set to: "+path)
+    host_ip=socket.gethostbyname_ex(socket.gethostname())[-1][0]
+    list.append(host_ip)
+    print("Server IP adress: "+host_ip)
+    username = os.getlogin()
+    list.append(username)
+    print("Server user: "+username)
     write_json(list)
-
+else:
+    print("Everything is already set up")
