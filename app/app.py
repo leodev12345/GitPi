7770567c1b3f33ee66eee9185ca74dd2ca0564a8
The Leo Developer <44649257+leodev12345@users.noreply.github.com>
2023-08-11 19:01:53 +0200

    rewrite old and bad code

--- app/app.py
+++ app/app.py
@@ -5,35 +5,24 @@ import bcrypt
 from functools import wraps
 import subprocess
 
-#Storing values locally
-list=[]
-config_list=[]
 #charecters not allowed in repo names
 ilegal_charecters=[" ","#","<",">","$","+","%","!","`","&","*","|","{","}","[","]","@",":","/","\"","\\","\'","\(","\)","=","?","€",";",".",",","§","¤","ß","Ł","ł","÷","×","¸","¨","~","ˇ","^","˘","°","˛","˙","´","˝"]
 #get current working path 
 current_path=os.path.abspath(os.getcwd())
-
+os.chdir(current_path)
 #load json values into the local array every time the program starts
 #chdir to project working folder to avoid loading database from wrong path when the working dir changes
-os.chdir(current_path)
-f=open("database/data.json")
-data=json.load(f)
-for i in data:
-    list.append(tuple((i[0],i[1],i[2],i[3])))
-
-#load program config from json into local array
-with open("database/config.json", "r") as f:
-    config_data=json.load(f)
-
-#useful variables
-config_password=config_data[0]
-config_path=config_data[1]
-config_ip=config_data[2]
-config_user=config_data[3]
+#function that returns contents of a specified json file
+def load_json(file):
+    with open(file, "r") as f:
+        return json.load(f)
+#load json into local dicts
+repo_dict=load_json("database/data.json")
+config_dict=load_json("database/config.json")
 
 #app init
 app = Flask(__name__)
-app.secret_key=config_data[4]
+app.secret_key=config_dict["app_secret_key"]
 
 #decorator that redirects to login page
 def login_required(func):
@@ -45,8 +34,8 @@ def login_required(func):
     return wrapper
 
 #function that writes all the repo data to data.json
-def write_json(arg):
-    json_object=json.dumps(arg, indent=4)
+def write_json(value):
+    json_object=json.dumps(value, indent=4)
     os.chdir(current_path)
     with open("database/data.json", "w") as file:
         file.write(json_object)
@@ -55,27 +44,26 @@ def write_json(arg):
 @app.route('/', methods=['POST'])
 def create_repo():
     #collecting info
-    text = request.form['text']
+    name = request.form['text']
     desc = request.form['desc']
     #checking info
-    if text!="" and any(ele in text for ele in ilegal_charecters)==False:
+    if name!="" and any(ele in name for ele in ilegal_charecters)==False:
         #set default description
         if desc=="":
             desc="No description"
-
         #full path to the repo
-        copy_text=config_user+"@"+config_ip+":"+config_path+text+".git"
-
+        path=f"{config_dict['server_user']}@{config_dict['server_IP']}:{config_dict['storage_path']}{name}.git"
+        
         #create the repo on the server
-        dir_name=text+".git"
-        os.chdir(config_path)
+        dir_name=f"{name}.git"
+        os.chdir(config_dict["storage_path"])
         os.mkdir(dir_name)
         os.chdir(dir_name)
         os.system("git init --bare")
         
         #add all the data to list and write it to data.json
-        list.insert(0,tuple((text,"Path: "+copy_text,desc,copy_text)))
-        write_json(list)
+        repo_dict[name]=[path, desc]
+        write_json(repo_dict)
 
     #refresh the website after form is submitted
     return redirect(url_for('create_repo'))
@@ -84,7 +72,7 @@ def create_repo():
 @app.route("/")
 @login_required
 def index_render():
-    return render_template('index.html', my_list=list)
+    return render_template('index.html', repos=dict(reversed(list(repo_dict.items()))))
 
 #render more options
 @app.route("/more")
@@ -102,7 +90,7 @@ def login_render():
 def login():
     #get the entered password and compare it to stored hashed password
     password=request.form['password']
-    result = bcrypt.checkpw(password.encode('utf-8'), config_password.encode('utf-8'))
+    result = bcrypt.checkpw(password.encode('utf-8'), config_dict["password"].encode('utf-8'))
     #log in if password is correct and display an error if not
     if result==True:
         session["logged-in"]=True
@@ -112,90 +100,77 @@ def login():
 
 
 # more options page functionality
+def rename_repo():
+    #get all the data
+    old_name = request.form['old_name']
+    new_name = request.form['new_name']
+    #check data
+    if old_name!=new_name and new_name!="" and old_name!="" and any(ele in new_name for ele in ilegal_charecters)==False:
+        #rename repo on server
+        os.chdir(config_dict["storage_path"])
+        os.rename(f"{old_name}.git",f"{new_name}.git")
+        #rename repo in local dict
+        repo_dict[new_name]=[f"{config_dict['server_user']}@{config_dict['server_IP']}:{config_dict['storage_path']}{new_name}.git", repo_dict[old_name][1]]
+        del repo_dict[old_name]
+        #write data to data.json
+        write_json(repo_dict)
+    return redirect(url_for("more"))
+
+def delete_repo():
+    #get all the data
+    delete_name = request.form['name_1']
+    delete_name_confirm = request.form['name_2']
+    #check data
+    if delete_name==delete_name_confirm and delete_name!="":
+        #delete from local dict
+        del repo_dict[delete_name]
+        #write data to data.json
+        write_json(repo_dict)
+    return redirect(url_for("more"))
+
+def change_repo_desc():
+    #get all the data
+    repo_name = request.form['repo_name']
+    new_desc = request.form['new_desc']
+    #check data
+    if repo_name!="" and new_desc!="":
+        #change data in local dict
+        repo_dict[repo_name][1]=new_desc
+        #write data to data.json
+        write_json(repo_dict)
+    return redirect(url_for("more"))
+
+def logout():
+    #log out the user and redirect back to login.html
+    session.pop("logged-in", None)
+    return redirect(url_for('login_render'))
+
+more_dict={
+    "rename": rename_repo,
+    "remove": delete_repo,
+    "confirm": change_repo_desc,
+    "logout": logout
+}
+
 @app.route("/more", methods=['POST'])
 def more():
-    #renaming repos
-    #check if the "rename" button is clicked
-    if 'rename' in request.form:
-        #get all the data
-        old_name = request.form['old_name']
-        new_name = request.form['new_name']
-        #check data
-        if old_name!=new_name and new_name!="" and old_name!="" and any(ele in new_name for ele in ilegal_charecters)==False:
-            #rename repo on server
-            os.chdir(config_path)
-            os.rename(old_name+".git",new_name+".git")
-
-            #get index of the tuple containing the old name
-            result = [tup[0] for tup in list].index(old_name)            
-            #get description that wont change
-            desc = list[result][2]
-            #change all the necessary data
-            path="Path: "+config_user+"@"+config_ip+":"+config_path+new_name+".git"
-            copy=config_user+"@"+config_ip+":"+config_path+new_name+".git" 
-            list[result]=tuple((new_name,path,desc,copy))
-            #write data to data.json
-            write_json(list)
-
-    #deleting repos
-    #check if "delete" button is pressed
-    elif 'remove' in request.form:
-        #get all the data
-        delete_name = request.form['name_1']
-        delete_name_confirm = request.form['name_2']
-        #check data
-        if delete_name==delete_name_confirm and delete_name!="":
-            #get index of the tuple that needs to be deleted
-            result = [tup[0] for tup in list].index(delete_name)
-            #delete the tuple from the list
-            del list[result] 
-            #write data to data.json
-            write_json(list)
-
-    #changing repo description
-    #check if "confirm" button is clicked
-    elif 'confirm' in request.form:
-        #get all the data
-        repo_name = request.form['repo_name']
-        new_desc = request.form['new_desc']
-        #check data
-        if repo_name!="" and new_desc!="":
-            #get the index of the tuple that contains the repo name
-            result = [tup[0] for tup in list].index(repo_name)
-            #get the values that wont change
-            name=list[result][0]
-            path=list[result][1]
-            copy=list[result][3]
-            #write new values
-            list[result] = tuple((name,path,new_desc,copy))
-            #write data to data.json
-            write_json(list)
-    
-    #logout
-    #check if "Log out" button is clicked
-    elif 'logout' in request.form:    
-        #log out the user and redirect back to login.html
-        session.pop("logged-in", None)
-        return redirect(url_for('login_render'))
-    
-    #refresh the website after form is submitted
-    return render_template('more.html')
+    for key in request.form:
+        if key in more_dict:
+            more_dict[key]()
+    return redirect(url_for("more"))
 
 @app.route("/<id>", methods=['GET', 'POST'])
 @login_required
 def repo(id):
     try:
-        path=config_path+id+".git"
+        path=f"{config_dict['storage_path']}{id}.git"
         os.chdir(path)
         files = subprocess.check_output(['git', 'ls-tree', '--name-only', '-r', 'HEAD']).decode('utf-8')
         commits = subprocess.check_output(['git', 'log', '--pretty=%B']).decode('utf-8').strip()
     except subprocess.CalledProcessError:
         files="No files are created yet"
         commits="No commits yet"
-    result = [tup[0] for tup in list].index(id)
-    desc = list[result][2]
-    path = list[result][3]
-    return render_template('repo.html', files=files, commits=commits, name=id, desc=desc, path=path)
+    return render_template('repo.html', files=files, commits=commits, name=id, desc=repo_dict[id][1], path=repo_dict[id][0])
 #run the program(debug only!)
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
--- app/config.py
+++ app/config.py
@@ -6,72 +6,63 @@ import subprocess
 import shutil
 
 #init variables
-list=[]
-list_data=[]
 prompt=0
-#loading config.json into local array
-with open("database/config.json", "r") as f:
-    list=json.load(f)
-#loading data.json into local array
-f=open("database/data.json")
-data=json.load(f)
-for i in data:
-    list_data.append(tuple((i[0],i[1],i[2],i[3])))
+#function that returns contents of a specified json file
+def load_json(file):
+    with open(file, "r") as f:
+        return json.load(f)
 
+config_dict=load_json("database/config.json")
+data_dict=load_json("database/data.json")
 #function that writes specified data to a specifiedjson
-def write_json(arg1, arg2):
-    json_object=json.dumps(arg1, indent=4)
-    with open(arg2, "w") as file:
-        file.write(json_object)
+def write_json(value, file):
+    json_object=json.dumps(value, indent=4)
+    with open(file, "w") as f:
+        f.write(json_object)
 #function that hashes specified password and returns it in a format capable of storing into json
-def hash_password(arg):
-    bytes = arg.encode('utf-8')
+def hash_password(password):
+    bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(bytes, salt)
     hash_json = hash.decode('utf8').replace("'", '"')
     return hash_json
 #function that changes specified values of all repositories in data.json
 def change_data(location, user):
-    for i in range(len(list_data)):
-        #get and change known values
-        name=list_data[i][0]
-        copy_text=user+"@"+list[2]+":"+location+name+".git"
-        path="Path: "+copy_text
-        desc=list_data[i][2]
-        #wite values to local array
-        list_data[i]=tuple((name, path, desc, copy_text))
+    for key in data_dict:
+        #change values in local dict
+        data_dict[key][0]=f"{user}@{config_dict['storage_path']}:{location}{key}.git"
 
 #first time setup
 def first_time_setup():
     os.system("clear") 
-    if len(list)==0:
+    if len(config_dict)==0:
         #password setup
         password=input("Create password: ")
-        list.append(hash_password(password)) 
-        print("Password set to "+password)
+        config_dict["password"]=hash_password(password)
+        print(f"Password set to {password}")
         #storage location setup
         path=input("Set storage location for your repositories: ")
         #add the / at the end of the path if the user didnt already add it
         if path[-1]!="/":
             path=path+"/"
-        list.append(path)
-        print("Storage path set to: "+path)
+        config_dict["storage_path"]=path
+        print(f"Storage path set to: {path}")
         #print host ip adress
         output=subprocess.check_output(['hostname', '-I']).decode('utf-8')
         host_ip=output.strip()
-        list.append(host_ip)
-        print("Server IP adress: "+host_ip)
+        config_dict["server_IP"]=host_ip
+        print(f"Server IP adress: {host_ip}")
         #user which will use the git server
         username=input("Set server user(leave blank for current linux user): ")
         if username=="":
             username = getpass.getuser()
-        list.append(username)
-        print("Server user: "+username)
+        config_dict["server_user"]=username
+        print(f"Server user: {username}")
         #generate secret key for flask sessions
         secret_key=os.urandom(24).hex()
-        list.append(secret_key)
+        config_dict["app_secret_key"]=secret_key
         #write everything to config.json
-        write_json(list, "database/config.json")
+        write_json(config_dict, "database/config.json")
         print("Setup complete!")
         print("\n", end="") 
         enter=input("Press ENTER to continue")
@@ -89,13 +80,13 @@ def change_password():
     while True:
         #if old password is entered correctly
         old_password=input("Enter current password: ")
-        result=bcrypt.checkpw(old_password.encode('utf-8'), list[0].encode('utf-8'))
+        result=bcrypt.checkpw(old_password.encode('utf-8'), config_dict["password"].encode('utf-8'))
         if result==True:
             new_password=input("Enter new password: ")
             #change password in local array and write changes to config.json
-            list[0]=hash_password(new_password)
-            write_json(list, "database/config.json")
-            print("password changed to: "+new_password)
+            config_dict["password"]=hash_password(new_password)
+            write_json(config_dict, "database/config.json")
+            print(f"password changed to: {new_password}")
             print("\n", end="")
             enter=input("Press ENTER to continue")
             os.system("clear")
@@ -110,24 +101,24 @@ def change_storage():
     #add / to the end of path
     if new_location[-1]!="/":
         new_location=new_location+"/"
-    prompt=input("All of your repositories will be moved from "+list[1]+" to "+new_location+" do you want to continue[y/n]? ")
+    prompt=input(f"All of your repositories will be moved from {config_dict['storage_path']} to {new_location} do you want to continue[y/n]? ")
     if prompt=="Y" or prompt=="y":
         #scan repository storage location and move all repositories from old path to new path
-        obj=os.scandir(list[1])
+        obj=os.scandir(config_dict["storage_path"])
         print("\n", end="")
         for entry in obj:
             if entry.is_dir() and ".git" in entry.name:
-                old=list[1]+entry.name
+                old=config_dict["storage_path"]+entry.name
                 new=new_location+entry.name
                 shutil.move(old, new)
-                print("Moved "+entry.name+" from "+list[1]+" to "+new_location)
+                print(f"Moved {entry.name} from {config_dict['storage_path']} to {new_location}")
         obj.close()
         #change vaules and write changes to data.json and config.json
-        list[1]=new_location
-        change_data(new_location, list[3])
-        print("Storage location changed to: "+new_location)
-        write_json(list, "database/config.json")
-        write_json(list_data, "database/data.json")
+        config_dict["storage_path"]=new_location
+        change_data(new_location, config_dict["server_user"])
+        print(f"Storage location changed to: {new_location}")
+        write_json(config_dict, "database/config.json")
+        write_json(data_dict, "database/data.json")
     print("\n", end="") 
     enter=input("Press ENTER to continue")
     os.system("clear")
@@ -140,11 +131,11 @@ def change_user():
     if new_user=="":
         new_user=getpass.getuser()
     #change values locally and write changes to json
-    list[3]=new_user
-    change_data(list[1], new_user)
-    write_json(list, "database/config.json")
-    write_json(list_data, "database/data.json")
-    print("Server user changed to: "+new_user)
+    config_dict["server_user"]=new_user
+    change_data(config_dict["storage_path"], new_user)
+    write_json(config_dict, "database/config.json")
+    write_json(data_dict, "database/data.json")
+    print(f"Server user changed to: {new_user}")
     print("\n", end="")
     enter=input("Press ENTER to continue")
     os.system("clear")
@@ -156,8 +147,8 @@ def delete_app_data():
     prompt=input("Are you sure you want to continue [y/n]? ")
     if prompt=="Y" or prompt=="y":
         #clear local array and overwrite all data with an empty array in data.json
-        list_data.clear()
-        write_json([],"database/data.json")
+        data_dict.clear()
+        write_json(data_dict,"database/data.json")
         print("Operation complete")
     print("\n", end="") 
     enter=input("Press ENTER to continue")
@@ -195,15 +186,14 @@ def change_config():
 #function that imports local repositories into the app (into data.json)
 def import_repos():
     os.system("clear")
-    print("Scanning "+list[1])
+    print("Scanning "+config_dict["storage_path"])
     print("\n", end="")
     #scan the storage location
-    obj=os.scandir(list[1])
+    obj=os.scandir(config_dict["storage_path"])
     to_import=[]
-    result = [tup[0] for tup in list_data]
     for entry in obj:
         #if folder ends with .git and doesent already exist in the app
-        if entry.is_dir() and ".git" in entry.name and any(ele in entry.name.replace(".git","") for ele in result)==False:
+        if entry.is_dir() and ".git" in entry.name and entry.name.replace(".git","") not in data_dict:
             print(entry.name)
             to_import.append(entry.name)
     obj.close()
@@ -217,19 +207,18 @@ def import_repos():
             #insert repository data into list_data
             for i in to_import:
                 name=i.replace(".git","")
-                path="Path: "+list[3]+"@"+list[2]+":"+list[1]+i
-                copy=list[3]+"@"+list[2]+":"+list[1]+i
-                print("Repository name: "+name)
+                path=f"{config_dict['server_user']}@{config_dict['server_IP']}:{config_dict['storage_path']}{i}"
+                print(f"Repository name: {name}")
                 #let the user create repository description which will be displayed in the app
                 desc=input("Create repository description(optional): ")
                 if desc=="":
                     desc="No description"
-                print("Repository description: "+desc)
-                print("Repository path: "+copy)
-                list_data.insert(0, tuple((name,path,desc,copy)))
+                print(f"Repository description: {desc}")
+                print(f"Repository path: {path}")
+                data_dict[name]=[path, desc]
                 print("\n", end="")
             #write new repository data into data.json
-            write_json(list_data, "database/data.json")
+            write_json(data_dict, "database/data.json")
             print("All repositories are successfully imported")
     else:
         print("All repositories are already imported/no repositories were detected")
@@ -241,12 +230,12 @@ def import_repos():
 def display_conf():
     os.system("clear") 
     #if user ran first time setup
-    if len(list)!=0:
+    if len(config_dict)!=0:
         print("Current configuration:")
         print("\n", end="")
-        print("Server IP adress: "+list[2])
-        print("Repositories storage location: "+list[1])
-        print("Git server user: "+list[3])
+        print(f"Server IP adress: {config_dict['server_IP']}")
+        print(f"Repositories storage location: {config_dict['storage_path']}")
+        print(f"Git server user: {config_dict['server_user']}")
     #if app isnt configured yet
     else:
         print("The app is not yet configured, run first time setup")
